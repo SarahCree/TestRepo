cab05f5635afa7b091242ab02b8bf4dd836d34e2
Sarah Cree <111320702+SarahCree@users.noreply.github.com>
2023-06-08 15:46:31 -0600

    Update to_csv.py

--- to_csv.py
+++ to_csv.py
@@ -4,7 +4,6 @@ import argparse
 import os
 
 def read_log(file_name, checkpoint):
-    test = 'test'
     try:
         with open ("logs/" + checkpoint + "/" + file_name) as f:
             data = f.read().split("\n")
